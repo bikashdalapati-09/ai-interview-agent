bf37f6663619b6ac78f5ea1c41cb460f62f48841
bikashdalapati-09 <bikashdalapati09@gmail.com>
2026-04-21 12:55:49 +0530

    feat: implement payment processing with Razorpay; add payment routes, controller, and model

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import cors from 'cors'
 import authRouter from '../routes/authRoute.js';
 import userRouter from '../routes/userRoute.js';
 import interviewRouter from '../routes/interviewRoute.js';
+import paymentRouter from '../routes/paymentRoute.js';
 
 const app = express();
 app.use(cors({
@@ -17,5 +18,6 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/interview", interviewRouter);
+app.use("/api/payment", paymentRouter);
 
 export default app
